a193bca3e5d61929af1ff22875df749f9eeafe3e
soumav <soumava91@gmail.com>
2021-08-06 16:24:32 +0530

    TestCommonFW project changes based on common-fw package changes

--- TestCommonFw/src/main/java/com/test/astracassandraapp/TestAstraCassandraApplication.java
+++ TestCommonFw/src/main/java/com/test/astracassandraapp/TestAstraCassandraApplication.java
@@ -3,7 +3,7 @@ package com.test.astracassandraapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.soumav.*", "com.test.*" })
+@SpringBootApplication(scanBasePackages = {"com.commonfw.*", "com.test.*" })
 public class TestAstraCassandraApplication {
 	
 	public static void main(String[] args) {
